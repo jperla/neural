068844b4e6d000f8e13d217bb0c710ff08e2956a
Joseph Perla <noootreeeeall@**jperla.com**>
2012-04-03 18:54:03 -0400

    beginnings of RAE encoder and parser

--- rae/scratch.py
+++ rae/scratch.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
 import numpy as np
 import scipy.io
+
+import neurolib
 
 matlab_filename = 'data/rt-polaritydata/RTData_CV1.mat'
 d = scipy.io.loadmat(matlab_filename)
 
 np.sum(d['test_ind'])
 
-def get_W(theta, has_Wcat, esize, cat_size, dictionary_length)
+def get_W(theta, has_Wcat, esize, cat_size, dictionary_length):
     """Accepts theta 1-d array of parameters,
         has_Wcat boolean of whether parameters have Wcat,bcat in it,
         esize integer size of embedded word vector size
@@ -52,7 +55,7 @@ def initialize_params(esize, vsize, cat_size, dictionary_length):
         Initialize parameters randomly based on layer sizes.
     """
     #We'll choose weights uniformly from the interval [-r, r]
-    r  = sqrt(6) / sqrt(esize + vsize + 1)
+    r  = np.sqrt(6) / np.sqrt(esize + vsize + 1)
 
     W1 = np.random.rand(esize, vsize) * 2 * r - r;
     W2 = np.random.rand(esize, vsize) * 2 * r - r;
@@ -72,3 +75,40 @@ def initialize_params(esize, vsize, cat_size, dictionary_length):
     # This step will "unroll" (flatten and concatenate together) all
     # your parameters into a vector, which can then be used with minFunc.
     return neurolib.flatten_params(W1, W2, W3, W4, b1, b2, b3, Wcat, bcat, We)
+
+
+def confusion_matrix(a, b):
+    """Accepts two 1-d arrays (N,)-shape with integer class labels.
+        Arrays must be of same size, and one predicts 
+            the other class labels.
+        Returns a 2-d array of NxN size.
+    """
+    N = a.shape[0]
+    assert (N,) == a.shape == b.shape
+
+    cm = np.zeros((N, N))
+    from itertools import izip
+    #TODO: jperla: might be faster with sparse matrices
+    for i, j in izip(a, b):
+        cm[i,j] += 1
+    return cm
+
+def  getAccuracy(predicted, gold):
+    """Accepts two 2-d arrays of predicted and gold-truth class label
+        integers from multinomial distribution.
+        Returns 4-tuple of reals of (precision, recall, accuracy, F1 score)
+    """
+    cm = confusion_matrix(gold, predicted)
+    n = len(gold)
+
+    rowsums = np.max(np.vstack([np.sum(cm, axis=0), np.repeat(1e-5, n)]),
+                     axis=0)
+    colsums = np.max(np.vstack([np.sum(cm, axis=1), np.repeat(1e-5, n)]),
+                     axis=0)
+
+    precision = np.mean(np.diag(cm) / colsums)
+    recall = np.mean(np.diag(cm) / rowsums)
+    accuracy = np.sum(np.diag(cm)) / np.sum(cm)
+    f1 = 2 * precision * recall / (precision + recall)
+    return precision, recall, accuracy, f1
+
